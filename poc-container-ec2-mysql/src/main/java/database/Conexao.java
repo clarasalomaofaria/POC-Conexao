e381abc53acb0d41e5f17ed37f423bccd718c340
clarasalomaofaria <clara.faria@sptech.school>
2023-05-07 22:55:11 -0300

    Atualizando o IP de conexão

--- poc-container-ec2-mysql/src/main/java/database/Conexao.java
+++ poc-container-ec2-mysql/src/main/java/database/Conexao.java
@@ -19,7 +19,7 @@ public class Conexao {
         BasicDataSource dataSource = new BasicDataSource();
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://54.163.211.188:3306/banco1");
+        dataSource.setUrl("jdbc:mysql://54.227.142.55:3306/banco1");
         dataSource.setUsername("root");
         dataSource.setPassword("#Gfteste");
 
